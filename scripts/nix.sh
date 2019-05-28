b8c1a5d25115f1c8754c5a62cd2881b2a1c88944
Erin Dahlgren <eedahlgren@gmail.com>
2019-05-28 13:48:37 -0700

    Update script

--- scripts/nix.sh
+++ scripts/nix.sh
@@ -2,3 +2,15 @@
 
 # Install nix
 curl https://nixos.org/nix/install | sh
+
+# Ensure that all necessary environment variables are set
+. $HOME/.nix-profile/etc/profile.d/nix.sh
+
+# Create a simple shell that includes gcc version 5
+nix run -f channel:nixos-17.09 gcc5
+gcc --version
+exit
+
+# Create another simple shell that includes gcc version 7
+gcc --version
+exit
